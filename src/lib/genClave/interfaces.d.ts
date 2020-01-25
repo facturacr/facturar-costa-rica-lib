4c49c01d4ba2b2000a7af5d72728a034e3a2ffb3
adrian <m.adrian15@gmail.com>
2020-01-25 11:22:33 -0600

    Ajustando interfaces de clave

--- src/lib/genClave/interfaces.d.ts
+++ src/lib/genClave/interfaces.d.ts
@@ -9,3 +9,25 @@ export interface ClaveOpts {
   sucursal: string;
   terminal: string;
 }
+
+export interface ClaveFecha { // 06 length
+  dia: string; // 02 lenght
+  mes: string; // 02 lenght
+  ano: string; // 02 lenght
+}
+
+export interface Consecutivo { // 20 lenght
+  sucursal: string; // 03 length
+  terminal: string; // 05 length
+  tipoDocumento: string; // 02 length
+  consecutivo: string; // 10 length
+}
+
+export interface Clave { // 50 lenght
+  codigoPais: string; // 03 length
+  fecha: ClaveFecha; // 06 length
+  cedulaEmisor: string; // 12 length
+  consecutivo: Consecutivo; // 20 lenght
+  situacionCE: string; // 1 lenght
+  codigoSeguridad: string; // 08 length
+}
