76b9c7cf75162cf3c9e51d70458fffe8e1276016
wainer <wainer.rodriguez@grupoardigital.com>
2020-11-13 21:43:36 -0600

    Adding jest

---
+++ __tests__/unit/getClave.test.ts
@@ -0,0 +1,11 @@
+import { getClave } from '../../src/lib/genClave'
+import fe from '../inputs/frontendRequest'
+import { FrontEndRequest } from '../../src/types/globalInterfaces'
+
+describe('GET CLAVE', () => {
+  it('should create a new key with 50 characters', () => {
+    const frontEndRequest: FrontEndRequest = fe
+    const result = getClave(frontEndRequest)
+    expect(result.length).toBe(50)
+  })
+})
--- test/getClave.ts
+++
@@ -1,9 +0,0 @@
-import { getClave } from '../src/lib/genClave'
-import fe from './input/frontendRequest'
-import { FrontEndRequest } from '../src/types/globalInterfaces'
-
-const frontEndRequest: FrontEndRequest = fe
-const result = getClave(frontEndRequest)
-
-console.log('Clave', result)
-console.log('pass test:', result.length === 50)
