f4c37173c3d7842dfa14e9946e7f9fcfc6be25ac
Minor Arrieta R <minor@xepelin.com>
2023-04-30 11:51:06 -0600

    feat(ATV): Exporting ATV

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import getToken from '@src/services/getToken'
 import genJSON from '@src/lib/genJSON'
 import eletronicBill from '@src/electronicBill'
 import * as genClaveTools from '@src/lib/genClave'
+import { ATV } from './ATV'
 // import creditNote from '@src/creditNote'
 // import debitNote from '@src/debitNote'
 
 export {
+  ATV,
   confirmXML,
   sendToCustomURL,
   getToken,
