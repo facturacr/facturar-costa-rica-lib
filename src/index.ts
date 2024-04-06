28c217a573594b85b24a7b1ff0eb111de19f99e4
Minor Arrieta Rojas <3359567+marnutux@users.noreply.github.com>
2024-04-06 09:43:26 -0600

    chore(Setup): Improve setup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,2 @@
-import confirmXML from '@src/confirmXML'
-import { sendToCustomURL } from '@src/services/send/index'
-import getToken from '@src/services/getToken'
-import genJSON from '@src/lib/genJSON'
-import eletronicBill from '@src/electronicBill'
-import * as genClaveTools from '@src/lib/genClave'
-import { ATV } from './ATV'
-// import creditNote from '@src/creditNote'
-// import debitNote from '@src/debitNote'
-
-export {
-  ATV,
-  confirmXML,
-  sendToCustomURL,
-  getToken,
-  eletronicBill,
-  genJSON,
-  genClaveTools
-}
+export { ATV } from './ATV'
+export { CreateDocumentInput } from './ATV/useCases/createDocument/types'
